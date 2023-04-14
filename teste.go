6be972303eae2fffd32c1f6c2ba0456878503514
PedroDiOliveira <pedro.h.soterio@gmail.com>
2023-04-13 23:38:42 -0300

    fimDosSequenciais

--- teste.go
+++ teste.go
@@ -4,12 +4,11 @@ import "fmt"
 
 func main() {
 	var n1 int
-	var n2 float64
-	fmt.Print("Qual é o primeiro numero?")
-	fmt.Scan(&n1)
-	fmt.Print("Qual é o segundo numero?")
-	fmt.Scan(&n2)
-	resultado := int(float64(n1) + n2)
-	fmt.Print(resultado)
+	var n2 int
 
+	if n1 > n2 {
+		fmt.Println("n1 é maior que n2")
+	} else {
+		fmt.Println("n2 é maior que n1")
+	}
 }
