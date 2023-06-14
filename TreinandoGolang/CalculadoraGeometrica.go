7b0286a1eb0560cd862d3c7da9e2d8a0b0431e20
PedroDiOliveira <pedr.h.soterio@gmail.com>
2023-06-13 23:18:32 -0300

    avanco

--- TreinandoGolang/CalculadoraGeometrica.go
+++ TreinandoGolang/CalculadoraGeometrica.go
@@ -2,76 +2,120 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"time"
 )
 
 func calculadoraSimples() float64 {
-	//declarando variaveis
+	// Declarando variáveis
 	var escolha int
 	var resultado float64
-	var x float64  //primeiro valor
-	var y float64  //segundo valor
-	var loope bool //cria um loope que caso o usuario nao digite um numero valido, ele retorna ao incio
+	var x float64  // primeiro valor
+	var y float64  // segundo valor
+	var loope bool // cria um loop que caso o usuário não digite um número válido, ele retorna ao início
 	loope = true
 
 	for loope {
-		//interface de escolha de calculo
-		fmt.Println("--Calculadora Simples--")
+		// Interface de escolha de cálculo
+		fmt.Println("------- Calculadora Simples-------")
 		fmt.Println()
-		fmt.Println("(1)Adição")
-		fmt.Println("(2)Subtração")
-		fmt.Println("(3)Multiplicação")
-		fmt.Println("(4)Divisão")
+		fmt.Println("(1) Adição")
+		fmt.Println("(2) Subtração")
+		fmt.Println("(3) Multiplicação")
+		fmt.Println("(4) Divisão")
 		fmt.Scan(&escolha)
 		loope = true
+		fmt.Println("")
+		fmt.Println("|-------------------------------------------|")
+		fmt.Println("")
 
 		switch escolha {
-		case 1: //soma
-			fmt.Println("Qual o primeiro numero?")
+		case 1: // Soma
+			fmt.Println("Qual o primeiro número?")
 			fmt.Scan(&x)
-			fmt.Println("Qual o segundo numero?")
+			fmt.Println("Qual o segundo número?")
 			fmt.Scan(&y)
 
 			resultado = x + y
-			fmt.Println("Calculando..")
+			fmt.Println("Calculando...")
 			time.Sleep(1 * time.Second)
-			fmt.Printf("O resultado é igual a %g ", resultado)
+			fmt.Printf("O resultado é igual a %g \n", resultado)
 			loope = false
-		case 2: //subtracao
-			fmt.Println("Qual o primeiro numero?")
+		case 2: // Subtração
+			fmt.Println("Qual o primeiro número?")
 			fmt.Scan(&x)
-			fmt.Println("Qual o segundo numero?")
+			fmt.Println("Qual o segundo número?")
 			fmt.Scan(&y)
 
 			resultado = x - y
-			fmt.Println("Calculando..")
+			fmt.Println("Calculando...")
 			time.Sleep(1 * time.Second)
-			fmt.Printf("O resultado é igual a %g ", resultado)
+			fmt.Printf("O resultado é igual a %g \n", resultado)
 			loope = false
-		case 3: //multiplicacao
-			fmt.Println("Qual o primeiro numero?")
+		case 3: // Multiplicação
+			fmt.Println("Qual o primeiro número?")
 			fmt.Scan(&x)
-			fmt.Println("Qual o segundo numero?")
+			fmt.Println("Qual o segundo número?")
 			fmt.Scan(&y)
 
 			resultado = x * y
-			fmt.Println("Calculando..")
+			fmt.Println("Calculando...")
 			time.Sleep(1 * time.Second)
-			fmt.Printf("O resultado é igual a %g ", resultado)
+			fmt.Printf("O resultado é igual a %g \n", resultado)
 			loope = false
-		case 4: //
+		case 4: // Divisão
 			fmt.Println("Qual o dividendo?")
 			fmt.Scan(&x)
 			fmt.Println("Qual o divisor?")
 			fmt.Scan(&y)
 
 			resultado = x / y
-			fmt.Println("Calculando..")
+			fmt.Println("Calculando...")
 			time.Sleep(1 * time.Second)
-			fmt.Printf("O resultado é igual a %g ", resultado)
+			fmt.Printf("O resultado é igual a %g \n", resultado)
 			loope = false
 		default:
-			fmt.Println("Voce nao digitou um numero valido.")
+			fmt.Println("Você não digitou um número válido.")
+		}
+	}
+	return resultado
+}
+
+func calculadoraGeometria() float64 {
+	var loope bool
+	var escolha int
+	loope = true
+	var raio float64
+	var resultado float64
+	for loope {
+		// Interface de escolha de cálculo
+		fmt.Println("-- Calculadora--Simples--")
+		fmt.Println()
+		fmt.Println("(1) Circulo")
+		fmt.Println("(2) Quadrado/Retangulo")
+		fmt.Println("(3) Triangulo")
+		fmt.Println("(4) Cilindro")
+		fmt.Scan(&escolha)
+		switch escolha {
+		case 1:
+			fmt.Println("Podemos calcular a area de um circulo multiplicando PI com o raio ao quadrado!")
+			fmt.Println("")
+			time.Sleep(3 * time.Second)
+			fmt.Println("OBS:Caso não tenha a medida do raio, basta pegar o diametro ou seja a medida de ponta a ponta e dividir por 2.")
+			fmt.Println("")
+			fmt.Println("|-------------------------------------------|")
+			time.Sleep(3 * time.Second)
+			fmt.Println("Sabendo disso:")
+			fmt.Println("Qual é o valor do raio em metros?")
+			fmt.Scan(&raio)
+
+			resultado = math.Pi * (raio * raio)
+
+			fmt.Println("Calculando...")
+			time.Sleep(1 * time.Second)
+
+			fmt.Printf("A área do circulo e igual a %.2f metros quadrados\n", resultado)
+			loope = false
 		}
 	}
 	return resultado
@@ -79,27 +123,34 @@ func calculadoraSimples() float64 {
 
 func main() {
 	fmt.Println("-------------------------------------------")
-	fmt.Println("---------Bem vindo(a) a calculadora--------")
+	fmt.Println("--------- Bem-vindo(a) à calculadora ------")
 	fmt.Println("-------------------------------------------")
 
 	time.Sleep(1 * time.Second)
 
-	//Declaracao de variaveis
-	var Materia int //qual calculadora o usuario vai escolher
+	// Declaração de variáveis
+	var Materia int // qual calculadora o usuário vai escolher
 
-	//recebendo o valora para Materia, para poder direcionar para a funcao desejada
+	// Recebendo o valor para Materia, para poder direcionar para a função desejada
 	fmt.Println("Qual assunto vamos abordar? ")
-	time.Sleep(2 * time.Second)
-	fmt.Println("(1)Calculos padrões")
-	fmt.Println("(2)Geometria")
-	fmt.Println("(3)Coming soon")
+	time.Sleep(1 * time.Second)
+	fmt.Println("(1) Cálculadora simples")
+	fmt.Println("(2) Geometria")
+	fmt.Println("(3) Coming soon")
 	fmt.Println("")
 	fmt.Scan(&Materia)
-
-	time.Sleep(1 * time.Second) // 2 segundos de delay para melhorar a interface do progama, deixando as informacoes mais organizadas
+	fmt.Println("")
+	fmt.Println("|-------------------------------------------|")
+	fmt.Println("")
 
 	switch Materia {
 	case 1:
 		calculadoraSimples()
+
+	case 2:
+		calculadoraGeometria()
 	}
+	fmt.Println("")
+	fmt.Println("Fechando progama..")
+	time.Sleep(1 * time.Second)
 }
